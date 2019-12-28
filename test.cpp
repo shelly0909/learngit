94572cb610454af626dd69270a7818fe096d68bd
shaoli <16slma@stu.edu.cn>
2019-12-28 22:38:34 +0800

    wrote a c++ test file

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main(){
+	char end;
+	int a[1010];
+	int i=0;
+    cin>>a[i];
+    cin>>end;
+    while(end!='#'){
+    	cin>>a[i++];
+    	cin>>end;
+	}
+	int k;
+	cin>>k;
+	for(int j=0;j<i;j++){
+		if(j+3<i){
+			for(int t=j+2;t>=j;t--){
+				cout<<a[t]<<"->";
+			}
+		}
+		else{
+			cout<<a[j]<<endl;
+		}
+	}
+	return 0;
+}
