1759592fb87a99cd9ad59b002ef183bd49387670
shaoli <16slma@stu.edu.cn>
2019-12-28 23:17:43 +0800

    git tracks changes of files

--- test.cpp
+++ test.cpp
@@ -29,4 +29,4 @@ int main(){
 //git is a distrributed version control system.
 //git is free software distributed under the GPL. 
 //git has a mutable index called stage.
-//git tracks changes.
+//git tracks changes of files.
